447f01be8a99449c8ba300048dcca78817e4fdb9
jpuig3-crypto <jpuig3@uoc.edu>
2026-06-09 11:28:34 +0300

    actualizacion completada

--- u3/u3e1.js
+++ u3/u3e1.js
@@ -3,3 +3,20 @@
 // Enunciado disponible en u3e1.md / Enunciat disponible a u3e1.md
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
+
+class Shape {
+
+    constructor(nSides = 0) {
+        if (new.target === Shape) {
+            throw new Error('ERROR_ABSTRACT. Abstract class Shape cannot be directly instantiated.');
+        }
+
+        this.nSides = nSides;
+    }
+
+    getArea() {
+        throw new Error('ERROR_ABSTRACT_METHOD. Method "getArea()" should be implemented by child classes of "Shape".');
+    }
+}
+
+export { Shape };
