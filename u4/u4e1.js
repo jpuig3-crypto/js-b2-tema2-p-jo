c4e37c2caf8754507d1dfe7f41029b60b3ee328a
jpuig3-crypto <jpuig3@uoc.edu>
2026-06-09 13:40:42 +0300

    actualizacion completada

--- u4/u4e1.js
+++ u4/u4e1.js
@@ -3,3 +3,41 @@
 // Enunciado disponible en u4e1.md / Enunciat disponible a u4e1.md
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
+
+const IPrintable = {
+    print: function() {}
+};
+
+Object.defineProperty(Object.prototype, 'implements', {
+    value: function (interf) {
+        for (let method in interf) {
+            if (typeof interf[method] === 'function') {
+                if (typeof this[method] !== 'function') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    },
+    enumerable: false,
+    writable: true,
+    configurable: true
+});
+
+class Message {
+    text;
+
+    constructor(textString) {
+        this.text = textString;
+
+        if (!this.implements(IPrintable)) {
+            throw new Error('ERROR_IMPLEMENTS. This class doesnt implements the interface IPrintable.');
+        }
+    }
+
+    print() {
+        console.log(this.text);
+    }
+}
+
+export { IPrintable, Message };
